3414a617561b4a63cdcfeeb8bc3e7cd1f83992d0
slayer3103 <saurabhshendurkar3103@gmail.com>
2026-05-24 19:58:16 +0530

    feat: initialize Prisma ORM with MongoDB configuration and basic database client setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
+export default async function Home() {
   return (
     <div className="flex justify-center items-center h-screen">
       <Button className="primary">
-        Get Started
+        Get Started 
       </Button>
     </div>
   );
