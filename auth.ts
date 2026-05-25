725fe0f894ba1cd1873702da1ea10454d1cd67e4
slayer3103 <saurabhshendurkar3103@gmail.com>
2026-05-25 12:38:39 +0530

    feat: fix auth secret

--- auth.ts
+++ auth.ts
@@ -87,5 +87,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   },
   secret: process.env.AUTH_SECRET,
   adapter: PrismaAdapter(db),
+  session:{ strategy: "jwt"},
   ...authConfig,
 });
